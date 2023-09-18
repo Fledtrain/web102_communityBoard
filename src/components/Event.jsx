1c37863fdb305011589f8eb8a6e137ddee3ad808
Fledtrain <Joeyew2014@gmail.com>
2023-09-18 16:58:37 -0400

    Added all ten games name, img and links to card

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,9 +3,15 @@ const Event = () => {
         <>
             <section className="container">
                 <Card img="Forza.jpg" name="Forza Motorsport" price={69.99} link="https://youtu.be/WtuBLc3cU-o?si=3ivQWAf41LD2V-um" />
-                <Card img="Forza.jpg" name="Star Wars Jedi: Survivor" price={69.99} link="https://youtu.be/WtuBLc3cU-o?si=3ivQWAf41LD2V-um" />
-                <Card img="Forza.jpg" name="Resident Evil 4" price={69.99} link="https://youtu.be/WtuBLc3cU-o?si=3ivQWAf41LD2V-um" />
-                <Card img="Forza.jpg" name="Resident Evil 4" price={69.99} link="https://youtu.be/WtuBLc3cU-o?si=3ivQWAf41LD2V-um" />
+                <Card img="SW.webp" name="Star Wars Jedi: Survivor" price={59.99} link="https://youtu.be/_F6YBwIPzmk?si=72c9zP4PCobKBa2m" />
+                <Card img="RE4.webp" name="Resident Evil 4" price={69.99} link="https://youtu.be/E69tKrfEQag?si=leYme_4T9wZqpoLp" />
+                <Card img="SM2.webp" name="Marvel's Spider-Man 2" price={79.99} link="https://youtu.be/XrPZSq5YXqc?si=vBi-YmO7rKGM2i-E" />
+                <Card img="HW.webp" name="Hogwarts Legacy" price={69.99} link="https://youtu.be/BtyBjOW8sGY?si=wiU5lCLebl0zKG5-" />
+                <Card img="TLZ.webp" name="The Legend of Zelda" price={69.99} link="https://youtu.be/uHGShqcAHlQ?si=_LYsG19rXJqG1DXO" />
+                <Card img="SOF.webp" name="Sons of the Forest" price={69.99} link="https://youtu.be/LpNDrrly3GI?si=EFmMU1Xasd5M9N5U" />
+                <Card img="CM.webp" name="Crew MotorFest" price={69.99} link="https://youtu.be/QuvaE7VhhjY?si=CQJdxScKLsQNKwel" />
+                <Card img="BG3.webp" name="Baldur's Gate 3" price={69.99} link="https://youtu.be/1T22wNvoNiU?si=c-EFXE6yvp72Fugw" />
+                <Card img="PM4.webp" name="Pikmin 4" price={69.99} link="https://youtu.be/RPiT-s4i0rw?si=mevkYTgd5SU8Pv6F" />
             </section>
         </>
     )
@@ -17,7 +23,7 @@ const Card = ({ img, name, price, link }) => {
     return (
         <>
             <aside className='card'>
-                <img className="logo" src={img} alt="Event" />
+                <img className="logo" src={img} alt="Event" loading="lazy" />
                 <div className="bundle">
                     <h5>{name}</h5>
                     <p>${price}</p>
